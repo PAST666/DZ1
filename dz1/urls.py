8122c0195a321a24f7b76483fdfda3ab245d15f0
alex6 <alex66651@yandex.ru>
2024-09-05 20:57:17 +0800

    поправили views

--- dz1/urls.py
+++ dz1/urls.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
 from django.urls import path
-from cabinet import views
+from cabinet.views import main, price, AppointmentView, license, gallery, preparation, reviews, ThanksView, get_services_by_master
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main, name="main"),
-    path("price/", views.price, name="price"),
-    path("appointment/", views.AppointmentView.as_view(), name="appointment"),
-    path("license/", views.license, name="license"),
-    path("gallery/", views.gallery, name="gallery"),
-    path("preparation/", views.preparation, name="preparation"),
-    path("reviews/", views.reviews, name="reviews"),
-    path("thanks_page/", views.ThanksView.as_view(), name="thanks_page"),
-    path("get_services_by_master/<int:master_id>/", views.get_services_by_master,
+    path('', main, name="main"),
+    path("price/", price, name="price"),
+    path("appointment/", AppointmentView.as_view(), name="appointment"),
+    path("license/", license, name="license"),
+    path("gallery/", gallery, name="gallery"),
+    path("preparation/", preparation, name="preparation"),
+    path("reviews/", reviews, name="reviews"),
+    path("thanks_page/", ThanksView.as_view(), name="thanks_page"),
+    path("get_services_by_master/<int:master_id>/", get_services_by_master,
     name="get_services_by_master"),
 ]
 
