fe5a210231f09b1ee28905938ca07b5fc3cac2fb
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-03-07 11:50:31 +0000

    Сериализаторы
    - Сериализатор `api` перенесен
    - Файл сериализатора `cabinet` удален

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from cabinet.models import Visit
+
+
+class VisitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Visit
+        fields = '__all__'
