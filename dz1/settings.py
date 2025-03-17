df40dcadd42619741ab33a14b085f8bfeaa023ee
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-03-17 13:29:50 +0000

    Миграции

--- dz1/settings.py
+++ dz1/settings.py
@@ -91,8 +91,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-STATIC_ROOT = BASE_DIR / "static"
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
